cf9094001b37a63793f47c228a119fdbdab4101e
Mohammad AlNajjar <monajjar@atypon.com>
2022-05-29 16:45:22 +0300

    fixes

--- core/pages.tsx
+++ core/pages.tsx
@@ -3,14 +3,17 @@ import { Routes as Switch, Route } from 'react-router-dom';
 import { Page as PageType } from '../types/page';
 import Page from './page';
 
-export default (props: { pages: PageType[] }) => {
-  const { pages } = props;
+export default (props: { pages: PageType[], boilerplate:boolean }) => {
+  const { pages, boilerplate } = props;
+  if (boilerplate) {
+       return <Page page={pages[0]}/>
+  }
   return (
     <Switch>
-      {pages.map((p: PageType) => {
+      {pages.map((p: PageType, key:number) => {
         return (
           <Route path={p.path}>
-            <Page page={p} />
+            <Page page={p} key={key}/>
           </Route>
         );
       })}
